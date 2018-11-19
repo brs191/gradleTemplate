6bd3d7e1cb42ccca946c5b06a3a45c0c5604328a
brs191 <rajashekar.b@gmail.com>
2018-11-19 10:53:34 +0900

    Add files via upload

---
+++ src/main/java/gradleTemplate.java
@@ -0,0 +1,9 @@
+
+import java.io.IOException;
+
+public class gradleTemplate {
+
+  public static void main(String[] argv) throws Exception {
+    System.out.println("this is raja gradleTemplate!!! ");
+  }
+}
